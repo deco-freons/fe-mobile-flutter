61ce6f8c11c4380d7294d32e1a8fbec78dfc6bfc
Yafi Ahsan Hakim <hakimy500@gmail.com>
2022-08-18 11:50:10 +0700

    feat: :wrench: Add register page and its route

---
+++ lib/page/register.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_boilerplate/common/components/forms/custom_form_input_class.dart';
+import 'package:flutter_boilerplate/common/components/forms/form_component.dart';
+import 'package:flutter_boilerplate/example/bloc/example_cubit.dart';
+import 'package:flutter_boilerplate/example/bloc/example_state.dart';
+import 'package:flutter_boilerplate/example/data/example_model.dart';
+import 'package:flutter_boilerplate/example/data/example_repository.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+  static const routeName = '/register';
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ExampleCubit(ExampleRepositoryImpl()),
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: Container(
+          color: Theme.of(context).colorScheme.primary,
+          child: SafeArea(child: buildRegister()),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRegister() {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 29.0, bottom: 45.0),
+          child: Center(
+              child:
+                  Image.asset('lib/common/assets/images/GlobeIconSmall.png')),
+        ),
+        BlocBuilder<ExampleCubit, ExampleState>(builder: (context, state) {
+          if (state is ExampleSuccessState) {
+            return const Text("success");
+          } else if (state is ExampleLoadingState) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+              ),
+            );
+          } else {
+            return RegisterForm();
+          }
+        }),
+      ],
+    );
+  }
+}
+
+class RegisterForm extends StatelessWidget {
+  RegisterForm({Key? key}) : super(key: key);
+
+  final CustomFormInput name =
+      CustomFormInput(label: 'First Name', type: "string");
+  final CustomFormInput email =
+      CustomFormInput(label: 'Email', type: "string", errorMessage: "error");
+  final CustomFormInput password = CustomFormInput(
+      label: 'Password', type: "password", errorMessage: "error");
+  final CustomFormInput date =
+      CustomFormInput(label: 'Birth Date', type: "date", errorMessage: "error");
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomForm(
+      title: 'Sign Up',
+      inputs: [name, email, password, date],
+      submitTitle: 'Create Account',
+      bottomText: 'Already have an account?',
+      textButton: "Sign In",
+      submitHandler: () {
+        ExampleModel data = ExampleModel(message: 'fullName');
+        submit(context, data);
+      },
+    );
+  }
+
+  void submit(BuildContext context, ExampleModel data) {
+    final cubit = context.read<ExampleCubit>();
+    cubit.healthcheck();
+  }
+}
