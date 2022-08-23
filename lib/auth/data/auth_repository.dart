e9c0a300cd8aed2bfa24de5582960061682d2518
Yafi Ahsan Hakim <hakimy500@gmail.com>
2022-08-23 13:29:45 +0700

    feat: :sparkles: Added logout cubit and integration

--- lib/auth/data/auth_repository.dart
+++ lib/auth/data/auth_repository.dart
@@ -6,10 +6,12 @@ import 'package:flutter_boilerplate/auth/login/data/login_model.dart';
 import 'package:flutter_boilerplate/common/config/enum.dart';
 import 'package:flutter_boilerplate/auth/data/user_model.dart';
 import 'package:flutter_boilerplate/common/data/base_repository.dart';
+import 'package:flutter_boilerplate/common/exception/internal_error_exception.dart';
 import 'package:flutter_boilerplate/common/utils/secure_storage..dart';
 
 abstract class AuthRepository implements BaseRepository {
   Future logIn(LoginModel model);
+  Future<void> logOut();
   Future<void> checkAuth();
   Stream<AuthModel> get status async* {}
   void dispose();
@@ -43,6 +45,23 @@ class AuthRepositoryImpl extends AuthRepository {
       _controller.add(AuthModel(user, AuthStatus.authenticated));
     } catch (e) {
       _controller.add(const AuthModel(null, AuthStatus.unauthenticated));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      String? refreshToken = await secureStorage.get(key: "refreshToken");
+      if (refreshToken == null) {
+        throw InternalServerErrorException();
+      }
+      await _authDataProvider.logout(refreshToken);
+      await secureStorage.set(key: "accessToken", value: "");
+      await secureStorage.set(key: "refreshToken", value: "");
+      await secureStorage.set(key: "user", value: "");
+      _controller.add(const AuthModel(null, AuthStatus.unauthenticated));
+    } catch (e) {
       rethrow;
     }
   }
