129c389908f36dd79198736a4ecaac7b9a01b88d
Oliver Muhammad Fadhlurrahman <olivermuhammadf@gmail.com>
2022-08-25 14:00:36 +1000

    feat: :sparkles: create homepage ui and small event card component

---
+++ lib/events/components/event_card_large.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventCardLarge extends StatefulWidget {
+  const EventCardLarge({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<EventCardLarge> createState() => _EventCardLargeState();
+}
+
+class _EventCardLargeState extends State<EventCardLarge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
