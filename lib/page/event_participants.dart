e4ac1d33f59a05a08f4b7650487b67c28a75ba48
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-09-01 12:32:18 +1000

    feat: :sparkles: Add Separate back button

---
+++ lib/page/event_participants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EventParticipants extends StatefulWidget {
+  const EventParticipants({Key? key}) : super(key: key);
+
+  @override
+  State<EventParticipants> createState() => _EventParticipantsState();
+}
+
+class _EventParticipantsState extends State<EventParticipants> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
