bbdb3635552164a676b7f5bd10895e5b34ca4feb
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-09-04 19:19:02 +1000

    refactor: change initial auth route to location permission

--- lib/app.dart
+++ lib/app.dart
@@ -3,11 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_boilerplate/auth/bloc/auth_bloc.dart';
 import 'package:flutter_boilerplate/auth/data/auth_repository.dart';
 import 'package:flutter_boilerplate/common/config/theme.dart';
-import 'package:flutter_boilerplate/page/event_detail.dart';
 import 'package:flutter_boilerplate/page/landing.dart';
 import 'package:flutter_boilerplate/page/get_started.dart';
-// import 'package:flutter_boilerplate/page/location_permission.dart';
-
+import 'package:flutter_boilerplate/page/location_permission.dart';
 import 'common/config/route_generator.dart';
 
 class App extends StatelessWidget {
@@ -58,10 +56,7 @@ class _AppViewState extends State<AppView> {
           listener: (context, state) {
             if (state is AuthAuthenticatedState) {
               _navigator.pushNamedAndRemoveUntil(
-                  // LocationPermission.routeName, (route) => false);
-                  EventDetail.routeName,
-                  (route) => false,
-                  arguments: 1);
+                  LocationPermission.routeName, (route) => false);
             } else if (state is AuthUnauthenticatedState) {
               _navigator.pushNamedAndRemoveUntil(
                   Landing.routeName, (route) => false);
