5beca09087d4b78f25a087d720095afddd23fd27
Oliver Muhammad Fadhlurrahman <olivermuhammadf@gmail.com>
2022-09-11 22:06:24 +1000

    feat: :sparkles: add sort to event search filter and modify filter modal to filter page

---
+++ lib/event/data/search_event/sort_event_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sort_event_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SortEventModel _$SortEventModelFromJson(Map<String, dynamic> json) =>
+    SortEventModel(
+      sortBy: json['sortBy'] as String,
+    );
+
+Map<String, dynamic> _$SortEventModelToJson(SortEventModel instance) =>
+    <String, dynamic>{
+      'sortBy': instance.sortBy,
+    };
