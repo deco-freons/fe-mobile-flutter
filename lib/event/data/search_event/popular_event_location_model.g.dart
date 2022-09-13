41f321acd7c9484a12ab15c562139ddb820680de
Yafi Ahsan Hakim <hakimy500@gmail.com>
2022-09-13 11:18:09 +0700

    refactor: :recycle: Large event card no longer use geocode

--- lib/event/data/search_event/popular_event_location_model.g.dart
+++ lib/event/data/search_event/popular_event_location_model.g.dart
@@ -1,27 +1,23 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'brisbane_location_model.dart';
+part of 'popular_event_location_model.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-BrisbaneLocationModel _$BrisbaneLocationModelFromJson(
+PopularEventLocationModel _$PopularEventLocationModelFromJson(
         Map<String, dynamic> json) =>
-    BrisbaneLocationModel(
-      location_id: json['location_id'] as int,
+    PopularEventLocationModel(
       suburb: json['suburb'] as String,
       city: json['city'] as String,
       state: json['state'] as String,
-      country: json['country'] as String,
     );
 
-Map<String, dynamic> _$BrisbaneLocationModelToJson(
-        BrisbaneLocationModel instance) =>
+Map<String, dynamic> _$PopularEventLocationModelToJson(
+        PopularEventLocationModel instance) =>
     <String, dynamic>{
-      'location_id': instance.location_id,
       'suburb': instance.suburb,
       'city': instance.city,
       'state': instance.state,
-      'country': instance.country,
     };
