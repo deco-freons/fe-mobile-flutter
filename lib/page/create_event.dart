db03dab3df803ced93dc0613ec35399c66e9c590
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-09-19 23:37:26 +1000

    refactor: :lipstick: fix loading indicator location

--- lib/page/create_event.dart
+++ lib/page/create_event.dart
@@ -37,58 +37,71 @@ class _CreateEventState extends State<CreateEvent> {
   }
 
   Widget buildCreateEvent() {
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 60.0, left: 20.0, right: 35.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 40.0),
-                child: TextButton(
-                  child: Text(
-                    "Cancel",
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.error),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: bodyPadding,
+              child: SizedBox(
+                height: appBarHeight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 40.0),
+                      child: TextButton(
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.error),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    const Text(
+                      'Create Event',
+                      style: TextStyle(
+                          fontSize: CustomFontSize.title,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
                 ),
               ),
-              const Text(
-                'Create Event',
-                style: TextStyle(
-                    fontSize: CustomFontSize.title,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
+            ),
+            BlocConsumer<CreateEventCubit, CreateEventState>(
+              builder: (context, state) {
+                if (state is CreateEventLoadingState) {
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height -
+                        MediaQuery.of(context).padding.top -
+                        appBarHeight -
+                        48,
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  );
+                } else if (state is CreateEventErrorState) {
+                  return CreateEventForm(errorMessage: state.errorMessage);
+                } else {
+                  return const CreateEventForm();
+                }
+              },
+              listener: (context, state) {
+                if (state is CreateEventSuccessState) {
+                  Navigator.pushReplacementNamed(context, Dashboard.routeName);
+                }
+              },
+            ),
+          ],
         ),
-        BlocConsumer<CreateEventCubit, CreateEventState>(
-          builder: (context, state) {
-            if (state is CreateEventLoadingState) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              );
-            } else if (state is CreateEventErrorState) {
-              return CreateEventForm(errorMessage: state.errorMessage);
-            } else {
-              return const CreateEventForm();
-            }
-          },
-          listener: (context, state) {
-            if (state is CreateEventSuccessState) {
-              Navigator.pushReplacementNamed(context, Dashboard.routeName);
-            }
-          },
-        ),
-      ],
+      ),
     );
   }
 }
